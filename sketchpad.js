d7e8c4f5f8050950a0796e9f500adc9dd107c9e3
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-12 11:43:20 -0500

    Implement random colour function

--- sketchpad.js
+++ sketchpad.js
@@ -2,12 +2,23 @@ const numPixels = 16 * 16
 const sideLength = 960/16
 const container = document.getElementById("container")
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function generateRandomColor(){
+    const redValue = getRandomInt(255)
+    const greenValue = getRandomInt(255)
+    const blueValue = getRandomInt(255)
+    return `rgb(${redValue}, ${greenValue}, ${blueValue})`
+}
+
 for (let i = 0; i < numPixels; i++){
     const grid = document.createElement("div")
     grid.style.boxSizing = "border-box"
     grid.style.width = sideLength + "px"
     grid.style.height = sideLength + "px"
     grid.style.backgroundColor = "blue"
-    grid.style.border = "solid"
+    grid.style.border = "solid 1px"
     container.appendChild(grid)
 }
